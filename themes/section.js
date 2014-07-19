fb6ba9c465de3683dd760575167b2c6ddfef2926
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-07-19 12:59:17 +0200

    Small style fixes in js

--- themes/section.js
+++ themes/section.js
@@ -1,64 +1,63 @@
-list = document.querySelectorAll('#controls a');
-for (var i=0; i<list.length ;i++) {
-  list[i].addEventListener("click", function(event) {
+for (var i = 0; i < document.querySelectorAll('aside.buttons a').length; i++) {
+  var link = document.querySelectorAll('aside.buttons a')[i];
+  link.setAttribute('title', link.innerHTML);
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
     role = this.getAttribute('data-role');
-    if ([
-      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote'
-    ].indexOf(role) >= 0) {
-        document.execCommand('formatBlock', false, role);
+    if (['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote'].indexOf(role) >= 0) {
+      document.execCommand('formatBlock', false, role);
     } else if (role == 'createLink') {
-        var range = window.getSelection().getRangeAt(0);
-        var content = range.extractContents();
-        var a = document.createElement('a');
-        a.appendChild(content);
-        var htmlContent = a.innerHTML;
-        document.execCommand(role, false, htmlContent);
-    }
-    else
-    {
+      var a = document.createElement('a');
+      a.appendChild(window.getSelection().getRangeAt(0).extractContents());
+      document.execCommand(role, false, a.innerHTML);
+    } else {
       document.execCommand(role, false, null);
     }
-    return false;
   });
 }
 
-for (var i=0; document.getElementsByTagName('section').length > i; i++) {
-  element = document.getElementsByTagName('section')[i];
-  element.setAttribute('contenteditable', 'true');
+for (var i = 0; i < document.querySelectorAll('nav.buttons a').length; i++) {
+  var link = document.querySelectorAll('nav.buttons a')[i];
+  link.setAttribute('title', link.innerHTML);
+}
+
+for (var i = 0; document.getElementsByTagName('section').length > i; i++) {
+  document.getElementsByTagName('section')[i].setAttribute('contenteditable', 'true');
 };
 
-document.getElementById('save').addEventListener("click", function(event) {
+document.getElementById('save').addEventListener('click', function(event) {
   var sections = '';
-  for (var i=0; document.getElementsByTagName('section').length > i; i++) {
+  for (var i = 0; document.getElementsByTagName('section').length > i; i++) {
     sections += document.getElementsByTagName('section')[i].outerHTML;
   };
 
   var request = new XMLHttpRequest();
-  request.open('POST', "{{ url_for('save', index=index, presentation=presentation) }}", true);
+  request.open('POST', '{{ url_for("save", index=index, presentation=presentation) }}', true);
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
   request.send('sections=' + sections);
   event.preventDefault();
   return false;
 });
 
-document.getElementById('add').addEventListener("click", function(event) {
+document.getElementById('add').addEventListener('click', function(event) {
   var parent = document.querySelector('div.slides');
-  var section = document.createElement("section");
-  var text = document.createTextNode("Write Here!");
-  section.appendChild(text);
+  var section = document.createElement('section');
   section.setAttribute('contenteditable', 'true');
+  section.innerHTML = '<h2>Title</h2><p>Content</p>';
   event.target = parent.insertBefore(section, document.querySelector('section.present').nextSibling);
   Reveal.next();
   event.preventDefault();
   return false;
 });
 
-document.getElementById("remove").addEventListener("click", function( event ) {
+document.getElementById('remove').addEventListener('click', function(event) {
   if (Reveal.isLastSlide() && Reveal.isFirstSlide()) {
     return false;
   }
   index = Reveal.getIndices().h;
-  if (Reveal.isLastSlide()) { index--; }
+  if (Reveal.isLastSlide()) {
+    index--;
+  }
   event.target = document.querySelector('section.present').remove();
   Reveal.slide(index, 0, 0);
   event.preventDefault();
